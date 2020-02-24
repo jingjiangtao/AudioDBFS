31d9e953a8f60cd01fac2e51e8cdeeba941576de
jingjiangtao <15210717655@163.com>
2020-02-24 18:00:23 +0800

    添加项目文件。

---
+++ AudioDBFS/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AudioDBFS
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string audioFile = @"C:\Users\jingj\mydata\temp\temp_audio\2\zhangwenhong.wav";
+            AudioDecibel.ReadAudioFile(audioFile);
+            Console.WriteLine("done");
+            Console.ReadKey();
+        }
+    }
+}
